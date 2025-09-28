7a69461366a72327947b30276802834009c39cf7
Pepper Ikeoka <pedroikeoka@gmail.com>
2025-09-28 19:07:28 -0300

    added basic turn functionality

---
+++ Assets/src/main/GameManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public enum TurnState {
+    START,
+    PLAYERS_TURN,
+    ENEMY_TURN,
+    WON,
+    LOST
+}
+
+public class Player {
+    public int playerIndex;
+    
+}
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager inst;
+    public TurnState turn;
+
+    public TextMeshProUGUI turnText;
+
+    public bool player1Ready = false;
+    public bool player2Ready = false;
+
+    public void PlayerHitEndTurn(int index) {
+        switch (index) {
+            case 1:
+                player1Ready = true;
+                break;
+            case 2:
+                player2Ready = true;
+                break;
+        }
+        CheckPlayersFinished();
+    }
+
+    private void CheckPlayersFinished() {
+        if (player1Ready && player2Ready) {
+            EndPlayerTurn();
+            player1Ready = false;
+            player2Ready = false;
+        }
+    }
+
+
+    private void EndPlayerTurn() {
+        if (turn == TurnState.PLAYERS_TURN) {
+            turn = TurnState.ENEMY_TURN;
+            Debug.Log("Enemy's Turn");
+            turnText.text = "ENEMY'S TURN";
+            // Enemy logic here
+            // After enemy turn ends, switch back to player's turn
+            StartCoroutine(EndEnemyTurn());
+        }
+    }
+
+    private IEnumerator EndEnemyTurn() {
+        yield return new WaitForSeconds(2f);
+        turn = TurnState.PLAYERS_TURN;
+        Debug.Log("Player's Turn");
+        turnText.text = "PLAYER'S TURN";
+    }
+
+    private void Awake() {
+        if (inst == null) {
+            inst = this;
+        } else if (inst != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        turn = TurnState.START;
+        turnText.text = "START";
+    }
+
+    private void FixedUpdate() {
+        if (turn == TurnState.START) {
+            turn = TurnState.PLAYERS_TURN;
+            Debug.Log("Player's Turn");
+            turnText.text = "PLAYER'S TURN";
+        }
+    }
+
+}
